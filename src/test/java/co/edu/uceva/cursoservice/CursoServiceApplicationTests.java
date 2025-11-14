22facf6f4fa86b73fd8725d9026ae2de73f3784a
Esteban <jaime.jurado01@uceva.edu.co>
2025-11-14 10:40:03 -0500

    second commit

---
+++ src/test/java/co/edu/uceva/cursoservice/CursoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.cursoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
